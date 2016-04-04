3e00dcb3633055fd0c5032a1d6eaf4c824840a33
Nate Hawthorne <natejhawthorne@gmail.com>
2016-04-03 23:15:34 -0500

    Adding a few arrays/objects

--- scripts/app.js
+++ scripts/app.js
@@ -44,3 +44,119 @@ album6.click(function(e) {
 	album6Overview.removeClass('inactive');
 });
 
+var album1Contents = [{
+	url: './images/spideymeme-1.jpg'
+}, {
+	url:'./images/spideyeme-2.jpg'
+}, {
+	url: './images/spideymeme-3.jpg'
+}, {
+	url: './images/spideymeme-4.jpg'
+}, {
+	url: './images/spideymeme-5.jpg'
+}, {
+	url: './images/spideymeme-6.png'
+}, {
+	url: './images/spideymeme-7.jpg'
+}, {
+	url: './images/spideymeme-8.jpg'
+}];
+
+var album2Contents = [{
+	url: './images/cat-bread-1.jpg'
+}, {
+	url: './images/cat-bread-2.jpg'
+}, {
+	url: './images/cat-bread-3.jpg'
+}, {
+	url: './images/cat-bread-4.jpg'
+}, {
+	url: './images/cat-bread-5.jpg'
+}, {
+	url: './images/cat-bread-6.jpg'
+}, {
+	url: './images/cat-bread-7.jpg'
+}, {
+	url: './images/cat-bread-8.jpg'
+}];
+
+var album3Contents = [{
+	url: './images/bad-charmander-1.jpg'
+}, {
+	url: './images/bad-charmander-2.jpg'
+}, {
+	url: './images/bad-charmander-3.png'
+}, {
+	url: './images/bad-charmander-4.jpg'
+}, {
+	url: './images/bad-charmander-5.jpg'
+}, {
+	url: './images/bad-charmander-6.png'
+}];
+
+var album4Contents = [{
+	url: './images/animal-names-1.png'
+}, {
+	url: './images/animal-names-2.png'
+}, {
+	url: './images/animal-names-3.png'
+}, {
+	url: './images/animal-names-4.png'
+}, {
+	url: './images/animal-names-5.png'
+}, {
+	url: './images/animal-names-6.png'
+}, {
+	url: './images/animal-names-7.png'
+}, {
+	url: './images/animal-names-8.png'
+}, {
+	url: './images/animal-names-9.png'
+}];
+
+var album5Contents = [{
+	url: './images/dumb-trump-1.jpg'
+}, {
+	url: './images/dumb-trump-2.jpg'
+}, {
+	url: './images/dumb-trump-3.jpg'
+}, {
+	url: './images/dumb-trump-4.jpg'
+}, {
+	url: './images/dumb-trump-5.jpg'
+}, {
+	url: './images/dumb-trump-6.jpg'
+}, {
+	url: './images/dumb-trump-7.jpg'
+}, {
+	url: './images/dumb-trump-8.jpg'
+}];
+
+var album6Contents = [{
+	url: './images/my-shows-1.jpg'
+}, {
+	url: './images/my-shows-2.jpg'
+}, {
+	url: './images/my-shows-3.jpg'
+}, {
+	url: './images/my-shows-4.jpg'
+}, {
+	url: './images/my-shows-5.jpg'
+}, {
+	url: './images/my-shows-6.png'
+}];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
